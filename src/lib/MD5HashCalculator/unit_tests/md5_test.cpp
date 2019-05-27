27b9b04f9e8e5b9bdabd0c9102d8b7ff432f7922
Alexander Chupikov <spider.ch@gmail.com>
2019-05-27 23:47:31 +0700

    Update tests for md5 algorithm.

--- src/lib/MD5HashCalculator/unit_tests/md5_test.cpp
+++ src/lib/MD5HashCalculator/unit_tests/md5_test.cpp
@@ -24,15 +24,61 @@ BOOST_AUTO_TEST_CASE(test_string_with_space)
 	Hash::MD5Hash hasher;
 	const std::string result = hasher.CalculateHash(data);
 
-	BOOST_CHECK_EQUAL(result, "7215ee9c7d9dcc29d2921a40e899ec5f");
+	BOOST_CHECK_EQUAL(result, "7215ee9c7d9dc229d2921a40e899ec5f");
 }
 
-//BOOST_AUTO_TEST_CASE(test_md5_string)
-//{
-//	const std::vector<char> data {'m', 'd', '5'};
-//
-//	Hash::MD5Hash hasher;
-//	const std::string result = hasher.CalculateHash(data);
-//
-//	BOOST_CHECK_EQUAL(result, "1bc29b36f623ba82aaf6724fd3b16718");
-//}
+BOOST_AUTO_TEST_CASE(test_md5_md5_string)
+{
+	const std::string str("md5");
+	const std::vector<unsigned char> data(str.cbegin(), str.cend());
+
+	Hash::MD5Hash hasher;
+	const std::string result = hasher.CalculateHash(data);
+
+	BOOST_CHECK_EQUAL(result, "1bc29b36f623ba82aaf6724fd3b16718");
+}
+
+BOOST_AUTO_TEST_CASE(test_md5_string_with_fox)
+{
+	const std::string string_data ("The quick brown fox jumps over the lazy dog");
+	const std::vector<unsigned char> data (string_data.cbegin(), string_data.cend());
+
+	Hash::MD5Hash hasher;
+	const std::string result = hasher.CalculateHash(data);
+
+	BOOST_CHECK_EQUAL(result, "9e107d9d372bb6826bd81d3542a419d6");
+}
+
+BOOST_AUTO_TEST_CASE(test_md5_string_with_alphabet)
+{
+	const std::string string_data ("abcdefghijklmnopqrstuvwxyz");
+	const std::vector<unsigned char> data (string_data.cbegin(), string_data.cend());
+
+	Hash::MD5Hash hasher;
+	const std::string result = hasher.CalculateHash(data);
+
+	BOOST_CHECK_EQUAL(result, "c3fcd3d76192e4007dfb496cca67e13b");
+}
+
+BOOST_AUTO_TEST_CASE(test_md5_string_with_alphabet_extended)
+{
+	const std::string string_data ("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+	const std::vector<unsigned char> data (string_data.cbegin(), string_data.cend());
+
+	Hash::MD5Hash hasher;
+	const std::string result = hasher.CalculateHash(data);
+
+	BOOST_CHECK_EQUAL(result, "d174ab98d277d9f5a5611c2c9f419d9f");
+}
+
+BOOST_AUTO_TEST_CASE(test_md5_long_string_with_numbers)
+{
+	const std::string string_data ("12345678901234567890123456789012345678901234567890123456789012345678901234567890");
+	const std::vector<unsigned char> data (string_data.cbegin(), string_data.cend());
+
+	Hash::MD5Hash hasher;
+	const std::string result = hasher.CalculateHash(data);
+
+	BOOST_CHECK_EQUAL(result, "57edf4a22be3c955ac49da2e2107b67a");
+}
+
