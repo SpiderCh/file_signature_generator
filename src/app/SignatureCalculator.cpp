e1910d878aba5a1896bd0406a19ece3743e230a5
Alexander Chupikov <spider.ch@gmail.com>
2019-05-31 12:03:43 +0700

    Calculate hash in several threads.

--- src/app/SignatureCalculator.cpp
+++ src/app/SignatureCalculator.cpp
@@ -1,5 +1,8 @@
 #include "SignatureCalculator.h"
 
+#include <future>
+#include <thread>
+#include <vector>
 #include <iostream>
 
 #include "IDataProvider.h"
@@ -36,15 +39,40 @@ CalculatorManager::~CalculatorManager() = default;
 void CalculatorManager::Start()
 {
 	size_t size = 0;
+	const unsigned int hardware_concurrency = std::thread::hardware_concurrency();
 	while(!m_impl->data_provider->eof())
 	{
-		const std::vector<char> data = m_impl->data_provider->Read(m_impl->bytes_to_read);
-		if (data.empty())
-			break;
+		std::vector<std::future<std::string>> workers;
+		std::vector<std::vector<std::uint8_t>> data;
 
-		size += data.size();
-		const std::string result = m_impl->hash_calculator->CalculateHash(data);
-		std::cout << "Read: " << data.size() << " Overall size: " << size << " Result: " << result <<std::endl;
+		for (unsigned int i = 0; i < hardware_concurrency; ++i)
+		{
+			data.push_back(m_impl->data_provider->Read(m_impl->bytes_to_read));
+			size += data.back().size();
+
+			std::packaged_task<std::string()> task([i, &data, this]
+			{
+				const std::string result = m_impl->hash_calculator->CalculateHash(data[i]);
+				return result;
+			});
+			workers.push_back(std::move(task.get_future()));
+			std::thread(std::move(task)).detach();
+			assert(workers.back().valid());
+
+			if (m_impl->data_provider->eof())
+				break;
+		}
+
+		for (std::future<std::string> & worker : workers)
+			worker.wait();
+
+		const size_t data_size = data.size();
+		for (size_t i = 0; i < data_size; ++i)
+		{
+			assert(workers[i].valid());
+			const std::string result = workers[i].get();
+			std::cout << "Read: " << data[i].size() << " Overall size: " << size << " Result: " << result << std::endl;
+		}
 	}
 }
 
